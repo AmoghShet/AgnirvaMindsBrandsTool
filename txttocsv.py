b14dac06db1f108a3d7e4283f4a27264957faa7e
Amogh Shet <amogh@agnirva.com>
2023-08-28 13:04:11 +0530

    Add files via upload

---
+++ txttocsv.py
@@ -0,0 +1,29 @@
+#EXTRACTS ONLY NAMES FROM THE .TXT FILE OF THE FORMAT "<Number>. <Name> - <Description>"
+
+import csv
+import re
+
+# Define the input and output file paths
+input_file = 'theemail.txt'
+output_csv_file = 'output.csv'
+
+# Regular expression pattern to extract names
+pattern = r'\d+\.\s+([^-\n]+)'
+
+# Create a list to store the extracted names
+names = []
+
+# Open and read the input file
+with open(input_file, 'r') as file:
+    for line in file:
+        match = re.search(pattern, line)
+        if match:
+            name = match.group(1).strip()
+            names.append([name])
+
+# Write the extracted names to a CSV file
+with open(output_csv_file, 'w', newline='') as csv_file:
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerows(names)
+
+print(f"Names extracted from '{input_file}' and saved in '{output_csv_file}'.")
